2320de0eb1c4f5b50852d065f9e25505ee1b0247
Andersen <actfire@gmail.com>
2021-03-22 15:04:08 +0800

    update

--- routes/test.js
+++ routes/test.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/').User;
+const User = require('../database/models/').User;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
